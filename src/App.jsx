33195709482216671de16e7b70b675cb3abbebe3
Anton Joshua Dominguez <143872285+antonjoshuadominguez@users.noreply.github.com>
2024-11-30 18:20:56 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import UserManagementPage from './pages/UserManagementPage';
+import DashboardPage from './pages/DashboardPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/user-management" element={<UserManagementPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
