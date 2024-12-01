27c13931bf74201e22b0f6d091d3a090e627b3c3
Anton Joshua Dominguez <143872285+antonjoshuadominguez@users.noreply.github.com>
2024-12-01 16:51:50 +0800

    Update AppSecurityConfig.java

--- backend/src/main/java/com/SulongEdukasyon/Sulong/Edukasyon/config/AppSecurityConfig.java
+++ backend/src/main/java/com/SulongEdukasyon/Sulong/Edukasyon/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.SulongEdukasyon.Sulong.Edukasyon.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
